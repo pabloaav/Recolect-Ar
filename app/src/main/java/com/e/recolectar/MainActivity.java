8a2efad6a53596e1c9a743232d2a468ce810b605
Pablo AV <pablovicentin84@gmail.com>
2019-04-22 22:31:49 +0000

    MainActivity.java editado a través de la web en Bitbucket

--- app/src/main/java/com/e/recolectar/MainActivity.java
+++ app/src/main/java/com/e/recolectar/MainActivity.java
@@ -2,7 +2,7 @@ package com.e.recolectar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/*pongo estas lineas para probar un update*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
