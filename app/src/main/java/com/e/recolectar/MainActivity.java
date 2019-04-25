f124a8236dca847aec6a8f576eba0e02c61815f7
alanb182 <alanboronat182@gmail.com>
2019-04-25 18:26:39 -0300

    Diseño de activities

--- app/src/main/java/com/e/recolectar/MainActivity.java
+++ app/src/main/java/com/e/recolectar/MainActivity.java
@@ -1,7 +1,10 @@
 package com.e.recolectar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 /*pongo estas lineas para probar un update*/
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    //Metodo Boton Siguiente
+    public void CrearNuevaCuenta(View view){
+        Intent crearNuevaCuenta = new Intent(this, CrearCuenta.class);
+        startActivity(crearNuevaCuenta);
+    }
+
 }
