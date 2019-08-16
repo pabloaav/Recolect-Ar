f9c72cb8131081238bbcc104910dc27f2469c8ab
pabloaav <pablovicentin84@gmail.com>
2019-08-16 18:09:53 -0300

    Ecopuntos dinamico con cardview y recycler view con datos desde Firebase

---
+++ app/src/main/java/com/e/recolectar/adaptadores/AdaptadorRecyclerEcopuntos.java
@@ -0,0 +1,116 @@
+package com.e.recolectar.adaptadores;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.e.recolectar.R;
+import com.e.recolectar.logica.modelo.EcopuntosPojo;
+
+import java.util.ArrayList;
+
+public class AdaptadorRecyclerEcopuntos extends RecyclerView.Adapter<AdaptadorRecyclerEcopuntos.EcopuntosViewHolder> {
+
+    //region ATRIBUTOS de AdaptadorRecyclerEcopuntos
+
+    //El arreglo de objetos Ecopuntos
+    ArrayList<EcopuntosPojo> listaEcopuntos;
+
+    //El layout del cardview donde se define que mostrar en pantalla y como
+    int cardViewLayout;
+
+    //El Context, que es la Activity desde donde se llama a mostrar los datos
+    Context mContext;
+
+    //endregion
+
+    //Constructor
+    public AdaptadorRecyclerEcopuntos(ArrayList<EcopuntosPojo> listaEcopuntos, int cardViewLayout, Context mContext) {
+        this.listaEcopuntos = listaEcopuntos;
+        this.cardViewLayout = cardViewLayout;
+        this.mContext = mContext;
+    }
+
+    //Este metodo toma los campos del cardview ecopuntos, y le asigna un valor determinado, que corresponde a cada objeto del arreglo de ecopuntos que viene dado como parametro de la clase AdaptadorRecyclerEcopuntos
+    @Override
+    public void onBindViewHolder(EcopuntosViewHolder ecopuntosViewHolder, int position) {
+        //En cada posicion del array hay un objeto ecopuntos.
+        EcopuntosPojo ecopunto = listaEcopuntos.get(position);
+
+        //De cada ecopunto del modelo de datos, que se alimenta de la base de datos, tomamos los valores de los atributos de cada objeto, y se los pasamos a la vista, para que en cada atributo del ViewHolder muestre su correspondiente valor
+        ecopuntosViewHolder.nombre.setText(ecopunto.getNombre());
+        ecopuntosViewHolder.ubicacion.setText(ecopunto.getUbicacion());
+        ecopuntosViewHolder.informacion.setText(ecopunto.getInformacion());
+
+    }//Fin de OnBind...
+
+
+    //Este metodo inicializa la vista del ViewHolder
+    @Override
+    public EcopuntosViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Decimos que desde el Context (la Activity Menu) inflar la vista de los cardview
+        View vista = LayoutInflater.from(mContext).inflate(cardViewLayout, parent, false);
+        //Le pasamos esta vista, es decir, el layout del cardview, a la Clase EcopuntosViewHolder, para que haga la asociacion de datos que le corresponde
+        return new EcopuntosViewHolder(vista);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (listaEcopuntos.size() > 0) {
+            return listaEcopuntos.size();
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Esta es la Clase Inner que se encarga de ligar los elementos del layout Cardview Ecopuntos
+     * que muestran los datos traidos de la base de datos, del nodo Ecopuntos
+     */
+    public class EcopuntosViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private TextView nombre;
+        private TextView ubicacion;
+        private TextView informacion;
+
+        /**
+         * Constructor: cada dato de un ecopunto, se asocia con los los elementos de la vista del cardview ecopuntos representados por su nombre de id que figuran en el layout Cardview
+         *
+         * @param itemView: Es el Cardview donde estan los elementos que vamos a asociar con los datos traidos de Firebase
+         */
+        public EcopuntosViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //Cada elemento del view (los cardview de ecopuntos) debe ser clicable
+            itemView.setOnClickListener(this);
+
+            //Aqui en el constructor, asociamos los valores del ecopunto tomados del cardview a sus atributos, declarados mas arriba, luego de la firma de la Clase Inner EcopuntosViewHolder
+            nombre = itemView.findViewById(R.id.cv_nombre);
+            ubicacion = itemView.findViewById(R.id.cv_ubicacion);
+            informacion = itemView.findViewById(R.id.cv_informacion);
+
+        }
+
+        //Al hacer clic en un cardview debe llevarnos a una vista ampliada
+        @Override
+        public void onClick(View view) {
+            //Vista correspondiente al click en el cardview de incidencia
+//            llamarWeb(view);
+
+        }
+
+        private void llamarWeb(View v) {
+//        Uri gmmIntentUri = Uri.parse("google.navigation:q=Plaza+Libertad");
+            String paginaWeb = "https://ecoplas.org.ar/reciclado-de-plasticos-2/";
+            Uri web = Uri.parse(paginaWeb);
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, web);
+            v.getContext().startActivity(webIntent);
+
+        }
+    }
+}
--- app/src/main/java/com/e/recolectar/fragmentos/PuntoReciclajeFragment.java
+++ app/src/main/java/com/e/recolectar/fragmentos/PuntoReciclajeFragment.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,11 +17,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.e.recolectar.R;
+import com.e.recolectar.adaptadores.AdaptadorRecyclerEcopuntos;
+import com.e.recolectar.adaptadores.AdaptadorRecyclerIncidencias;
+import com.e.recolectar.logica.modelo.EcopuntosPojo;
+import com.e.recolectar.logica.modelo.IncidenciaPojo;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link PuntoReciclajeFragment.OnFragmentInteractionListener} interface
+ * {@link OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link PuntoReciclajeFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -39,7 +55,9 @@ public class PuntoReciclajeFragment extends Fragment {
     //endregion
 
     //region Atributos
-    private TextView txt_ubicacion;
+    private TextView tv_ubicacion;
+    private DatabaseReference mDataBase;
+    private FirebaseAuth mAuth;
     //endregion
 
     //region Metodos por Default
@@ -109,22 +127,61 @@ public class PuntoReciclajeFragment extends Fragment {
     //region Metodos
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        }
+
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        final View vista = inflater.inflate(R.layout.fragment_punto_reciclaje, container, false);
-        //hacer el bind del textview de la ubicacion del ecopunto
-        txt_ubicacion = vista.findViewById(R.id.tv_Ubicacion);
-        txt_ubicacion.setTextColor(getResources().getColor(R.color.linkColor));
-        //Lo hacemos clickable
-        txt_ubicacion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Hacer lo del maps app
-                llamarMapsApp();
-              Toast.makeText(getActivity(), "Abriendo Ecopunto en Maps de Google", Toast.LENGTH_SHORT).show();
+        //Inflamos la vista del layout puntos de reciclaje
+        View vista = inflater.inflate(R.layout.fragment_punto_reciclaje, container, false);
 
-            }
-        });
+        //Binding de elementos del layout
+//        tv_ubicacion = vista.findViewById(R.id.tv_Ubicacion);
+
+        // Objetos de Firebase necesarios para referenciar DB y autenticacion
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        mDataBase = firebaseDatabase.getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        //Instanciamos el objeto Recycler view
+        RecyclerView recyclerViewEcopuntos = vista.findViewById(R.id.rv_ecopuntos);
+
+        //Se necesita un objeto Linear Layaout Manager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        //Seteamos el recycler con el Layout Manager
+        recyclerViewEcopuntos.setLayoutManager(linearLayoutManager);
+
+        //Cargamos el array de ecopuntos con los datos de la base de datos
+        cargarArrayRecycler(recyclerViewEcopuntos);
+
+//        //hacer el bind del textview de la ubicacion del ecopunto
+//        tv_ubicacion = vista.findViewById(R.id.tv_Ubicacion);
+//        tv_ubicacion.setTextColor(getResources().getColor(R.color.linkColor));
+
+        //Lo hacemos clickable
+//        tv_ubicacion.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //Hacer lo del maps app
+//                llamarMapsApp();
+//                Toast.makeText(getActivity(), "Abriendo Ecopunto en Maps de Google", Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+
         // Inflate the layout for this fragment
         return vista;
 
@@ -140,17 +197,40 @@ public class PuntoReciclajeFragment extends Fragment {
         }
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    private void cargarArrayRecycler(final RecyclerView rvEcopuntos) {
 
-        }
+        DatabaseReference ref = mDataBase.child("Ecopuntos");
 
-    }
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
+                ArrayList<EcopuntosPojo> array = new ArrayList<>();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    //Se crea objeto desde el snapshot de la base de datos con los datos de cada ecopunto
+                    EcopuntosPojo ecopunto = snapshot.getValue(EcopuntosPojo.class);
+                    ecopunto.setNombre(ecopunto.getNombre());
+                    ecopunto.setUbicacion(ecopunto.getUbicacion());
+                    ecopunto.setInformacion(ecopunto.getInformacion());
+                    array.add(ecopunto);
+
+                }//Fin del for
+
+                //Colocamos la clase adaptadora del recycler que creamos para instanciar el view holder y manejar el objeto RecyclerView. El primer parametro que recibe el constructor debe ser un array de objetos de Ecopuntos que se obtienen de la base de datos
+                AdaptadorRecyclerEcopuntos adaptador = new AdaptadorRecyclerEcopuntos(array, R.layout.cardview_ecopuntos, getActivity());
+
+                //Seteamos al recycler el adaptador correspondiente
+                rvEcopuntos.setAdapter(adaptador);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }//Fin cargar recycler
 
     //endregion
 
