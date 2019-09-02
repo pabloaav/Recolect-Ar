6e906d51a72d06cf786884b97b79f5259f6d50e6
pabloaav <pablovicentin84@gmail.com>
2019-09-02 00:03:58 -0300

    implementado conteo estadistico de usos. se cambia de lugar ecopuntos cardview

--- app/src/main/java/com/e/recolectar/logica/vista/SplashScreen.java
+++ app/src/main/java/com/e/recolectar/logica/vista/SplashScreen.java
@@ -3,6 +3,7 @@ package com.e.recolectar.logica.vista;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -11,6 +12,11 @@ import com.e.recolectar.R;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -20,11 +26,11 @@ public class SplashScreen extends AppCompatActivity {
     //region Atributos
     private FirebaseAuth firebaseAuth;
     private ProgressBar mProgressBar;
-    //endregion
-
-
     ProgressBar pb;
     int counter = 10;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    //endregion
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,12 +60,8 @@ public class SplashScreen extends AppCompatActivity {
 
         t.schedule(tt,0,25);
 
-
-
         //inicializacion de Firebase
         inicializarFirebase();
-
-
 
     }//Fin de onCreate()
 
@@ -76,6 +78,8 @@ public class SplashScreen extends AppCompatActivity {
                     Intent menu = new Intent(SplashScreen.this, MenuInicio.class);
                     menu.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
                     startActivity(menu);
+                    //contamos la estadistica "usos":
+                    contarUso();
                     finish();
                 } else {
                     Intent login = new Intent(SplashScreen.this, MainActivity.class);
@@ -88,12 +92,33 @@ public class SplashScreen extends AppCompatActivity {
         }, 2000);
     }//Fin de onStart()
 
+    private void contarUso() {
+        final DatabaseReference ref = databaseReference.child("Estadisticas/Usuarios/usos");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snap) {
+                Integer usos = snap.getValue(Integer.class);
+                usos++;
+                ref.setValue(usos);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     //Inicializacion de objetos de Firebase
     private void inicializarFirebase() {
         FirebaseApp.initializeApp(this);
         //inicializamos el objeto firebaseAuth
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
     }
+
+
 
 }
 
